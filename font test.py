f989953be480afc51380258970dc03f34e78016d
Sheila <smercado2468@gmail.com>
2026-05-06 17:51:18 -0400

    removed font test file

--- font test.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-# This gets the list of all available system font names
-all_fonts = pygame.font.get_fonts()
-
-# Print them all to your terminal
-for font_name in all_fonts:
-    print(font_name)
